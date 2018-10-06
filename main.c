9267f7dc038c5b2c21c604c8e9b39c26cfa0a2f1
ecwu <bitlabwzh@gmail.com>
2018-10-06 23:42:34 +0800

    finish the basic function

--- main.c
+++ main.c
@@ -1,29 +1,57 @@
+#include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+
 #define SIZE 1024
+
 int main(void) {
     char prompt[] = "Type a command: ";
     char buf[SIZE];
-    // Ask the user to type a command (write to standard output stream):
-    write(1, prompt, sizeof(prompt)-1);
-    // Read from the standard input the command typed by the user (note
-    // that fgets also puts into the array buf the ’\n’ character typed
-    // by the user when the user presses the Enter key on the keyboard):
-    // Read from standard input stream (fd == 0)
-    read(0, buf, SIZE);
-    // Replace the Enter key typed by the user with ’\0’:
-    for(int i = 0; i < SIZE; i++) {
-        if (buf[i] == '\n' || buf[i] == '\r') {
-            buf[i] = '\0';
-            break;
+    pid_t child_pid; // PID of child process
+    int status; // Exit status of child process
+    while (1) {
+        // Ask the user to type a command (write to standard output stream):
+        write(1, prompt, sizeof(prompt) - 1);
+        // Read from the standard input the command typed by the user (note
+        // that fgets also puts into the array buf the ’\n’ character typed
+        // by the user when the user presses the Enter key on the keyboard):
+        // Read from standard input stream (fd == 0)
+        read(0, buf, SIZE);
+        // Replace the Enter key typed by the user with ’\0’:
+        for (int i = 0; i < SIZE; i++) {
+            if (buf[i] == '\n' || buf[i] == '\r') {
+                buf[i] = '\0';
+                break;
+            }
+        }
+        if (strcmp(buf, "exit") == 0){
+            // if user input "exit", terminate the loop
+            char exit_info[] = "Exiting..\n";
+            write(1, exit_info, strlen(exit_info));
+            return 0;
+        }
+        child_pid = fork();
+        if (child_pid < 0) {
+            char error_info[] = "Parent: fork failed\n";
+            write(1, error_info, strlen(error_info));
+            return 1;
+        } else if (child_pid == 0) {
+            // Parent will never run this part
+            char child_running_info[] = "Child: now running same program as parent, doing exec\n";
+            write(1, child_running_info, strlen(child_running_info));
+            // Child process run the user command
+            execlp(buf, buf, NULL);
+            //
+            char error_info = "Child: exec failed, die\n";
+            write(1, error_info, strlen(error_info));
+            // Child process dies after the failed exec
+            return 1;
+        } else {
+            char wait_info[] = "Parent: now sleeping waiting for child to end\n";
+            write(1, wait_info, strlen(wait_info));
+            wait(&status);
+            char finish_info[] = "Parent: Finish wait\n";
+            write(1, finish_info, strlen(finish_info));
         }
     }
-    // prepare the variable to store the output content
-    char output_content[SIZE*2] = "Executing command: ";
-    // connect the input buffer with output_content string
-    strcat(output_content, buf);
-    // write to standard output stream (fd == 1)
-    write(1, output_content, strlen(output_content));
-    // add a line feed
-    write(1, "\n", 1);
-    return 0;
 }
