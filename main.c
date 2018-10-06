dce7832ade0146eee98615aa408ef0896202f1a3
ecwu <bitlabwzh@gmail.com>
2018-10-07 01:17:56 +0800

    add comments

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 #define SIZE 1024
 
@@ -12,9 +13,6 @@ int main(void) {
     while (1) {
         // Ask the user to type a command (write to standard output stream):
         write(1, prompt, sizeof(prompt) - 1);
-        // Read from the standard input the command typed by the user (note
-        // that fgets also puts into the array buf the ’\n’ character typed
-        // by the user when the user presses the Enter key on the keyboard):
         // Read from standard input stream (fd == 0)
         read(0, buf, SIZE);
         // Replace the Enter key typed by the user with ’\0’:
@@ -30,6 +28,7 @@ int main(void) {
             write(1, exit_info, strlen(exit_info));
             return 0;
         }
+        // create child process
         child_pid = fork();
         if (child_pid < 0) {
             char error_info[] = "Parent: fork failed\n";
@@ -41,16 +40,16 @@ int main(void) {
             write(1, child_running_info, strlen(child_running_info));
             // Child process run the user command
             execlp(buf, buf, NULL);
-            //
-            char error_info = "Child: exec failed, die\n";
+            char error_info[] = "Child: exec failed, die\n";
             write(1, error_info, strlen(error_info));
             // Child process dies after the failed exec
             return 1;
         } else {
             char wait_info[] = "Parent: now sleeping waiting for child to end\n";
             write(1, wait_info, strlen(wait_info));
+            // wait for child to end
             wait(&status);
-            char finish_info[] = "Parent: Finish wait\n";
+            char finish_info[] = "Parent: Finish wait for child, child is dead\n";
             write(1, finish_info, strlen(finish_info));
         }
     }
