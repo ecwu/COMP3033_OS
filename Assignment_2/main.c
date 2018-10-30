07e70539f6df3c2122da00c25ebd50850e682a48
ecwu <bitlabwzh@gmail.com>
2018-10-30 22:34:12 +0800

    upload version: auto indent

--- Assignment_2/main.c
+++ Assignment_2/main.c
@@ -26,7 +26,7 @@ int main() {
     printf("Enter the number of processes to schedule: ");
     scanf("%d", &NumOfProcesses);
 
-    float cpuBurst = 0, totalWaiting=0;
+    float cpuBurst = 0, totalWaiting = 0;
 
     // Create a pcb array using dynamic memory allocation
     pcb *ProcessInfoArray = (pcb *) malloc(sizeof(pcb) * NumOfProcesses);
@@ -58,16 +58,18 @@ int main() {
 
         int operate = 0;
 
-        for (int k = 0; k < NumOfProcesses ; k++) {
-            if (ProcessInfoArray[k].remaining_time == 0 && i % ProcessInfoArray[k].period == 0){
+        for (int k = 0; k < NumOfProcesses; k++) {
+            if (ProcessInfoArray[k].remaining_time == 0 &&
+                i % ProcessInfoArray[k].period == 0) {
                 operate = 4; // If meet a period's start
                 break;
             }
-            if (i > 0){ // When a process is overdue
+            if (i > 0) { // When a process is overdue
                 if (ProcessInfoArray[k].remaining_time > 0 &&
-                ProcessInfoArray[k].remaining_time < ProcessInfoArray[k].burst_time &&
-                i % ProcessInfoArray[k].period == 0
-                ){
+                    ProcessInfoArray[k].remaining_time <
+                    ProcessInfoArray[k].burst_time &&
+                    i % ProcessInfoArray[k].period == 0
+                        ) {
                     operate = 5; // If meet a period's start and also some process's remaining time is greater than 0
                     break;
                 }
@@ -75,26 +77,26 @@ int main() {
         }
 
 
-        if (operate < 4){ // If the status is non of them above
-            if(nextProcess == -1){ // If currently don't have process
+        if (operate < 4) { // If the status is non of them above
+            if (nextProcess == -1) { // If currently don't have process
                 operate = 1; // Pick next process
-            } else if (ProcessInfoArray[nextProcess].remaining_time > 0){
+            } else if (ProcessInfoArray[nextProcess].remaining_time > 0) {
                 // If a process is running and remaining_time greater than 0 (still running)
                 operate = 2; // Running the Process
-            } else if (ProcessInfoArray[nextProcess].remaining_time == 0){
+            } else if (ProcessInfoArray[nextProcess].remaining_time == 0) {
                 // If the remaining_time go to 0 (finished running)
                 operate = 3; // Finish a Process
             }
 
             int FinishedInPeriod = 0; // Number Of the Process finished within the period
-            for(int j = 0; j < NumOfProcesses; j++){
-                if (ProcessInfoArray[j].remaining_time == 0){
+            for (int j = 0; j < NumOfProcesses; j++) {
+                if (ProcessInfoArray[j].remaining_time == 0) {
                     FinishedInPeriod++;
                 }
             }
             if (FinishedInPeriod == NumOfProcesses) {
                 operate = 0; // idle the cpu
-                if (nextProcess != -1){ // no process before idle
+                if (nextProcess != -1) { // no process before idle
                     operate = 3;
                 }
             }
@@ -112,50 +114,56 @@ int main() {
                         nextProcess = l;
                     }
                 }
-                for (int j = 0; j < NumOfProcesses; j++) { // Find the Earliest Deadline Process
+                for (int j = 0; j <
+                                NumOfProcesses; j++) { // Find the Earliest Deadline Process
                     // Pick the next running process
                     if (ProcessInfoArray[j].remaining_time > 0) {
                         if (ProcessInfoArray[j].period -
-                            i % ProcessInfoArray[j].period<
+                            i % ProcessInfoArray[j].period <
                             ProcessInfoArray[nextProcess].period -
                             i % ProcessInfoArray[nextProcess].period) {
                             nextProcess = j;
                         }
                     }
                 }
-                printf("%d: process %d starts\n", i, nextProcess+1); // Print the start message
+                printf("%d: process %d starts\n", i,
+                       nextProcess + 1); // Print the start message
                 break;
             case 2:
                 i++; // increase total running time
                 ProcessInfoArray[nextProcess].remaining_time -= 1; // minus the remaining_time
                 for (int k = 0; k < NumOfProcesses; k++) {
-                    if (k != nextProcess && ProcessInfoArray[k].remaining_time > 0){
+                    if (k != nextProcess &&
+                        ProcessInfoArray[k].remaining_time > 0) {
                         // count waiting time for waiting process
                         totalWaiting++;
                     }
                 }
                 break;
             case 3:
-                printf("%d: process %d end\n", i, nextProcess+1); // Print the end message
+                printf("%d: process %d end\n", i,
+                       nextProcess + 1); // Print the end message
                 nextProcess = -1; // Reset the next process
                 break;
             case 4: // Meet Period
                 // Update remaining time
-                if (nextProcess != -1){
-                    if (i % ProcessInfoArray[nextProcess].period == 0 && ProcessInfoArray[nextProcess].remaining_time == 0){
+                if (nextProcess != -1) {
+                    if (i % ProcessInfoArray[nextProcess].period == 0 &&
+                        ProcessInfoArray[nextProcess].remaining_time == 0) {
                         // the process finished but not end
-                        printf("%d: process %d end\n", i, nextProcess+1); // Print the end message
+                        printf("%d: process %d end\n", i,
+                               nextProcess + 1); // Print the end message
                         nextProcess = -1; // Reset the next process
                         break;
                     }
                 }
 
-                for(int j = 0; j < NumOfProcesses; j++){
-                    if (i % ProcessInfoArray[j].period == 0){
+                for (int j = 0; j < NumOfProcesses; j++) {
+                    if (i % ProcessInfoArray[j].period == 0) {
                         ProcessInfoArray[j].remaining_time += ProcessInfoArray[j].burst_time; // add burst time to every process that meet it's period
                     }
                 }
-                if (nextProcess != -1){
+                if (nextProcess != -1) {
                     int urgentProcess = nextProcess;
                     for (int j = 0; j < NumOfProcesses; j++) {
                         // Pick the a more urgent process
@@ -173,12 +181,13 @@ int main() {
                     int maxRemainingTimeProcess = -1;
                     // When the current process have another process that have the same deadline but different remaining_time
                     for (int j = 0; j < nextProcess; j++) {
-                        if(ProcessInfoArray[nextProcess].remaining_time < ProcessInfoArray[j].remaining_time){
+                        if (ProcessInfoArray[nextProcess].remaining_time <
+                            ProcessInfoArray[j].remaining_time) {
                             if (ProcessInfoArray[j].period -
-                            i % ProcessInfoArray[j].period ==
-                            ProcessInfoArray[nextProcess].period -
-                            i % ProcessInfoArray[nextProcess].period)
-                            maxRemainingTimeProcess = j;
+                                i % ProcessInfoArray[j].period ==
+                                ProcessInfoArray[nextProcess].period -
+                                i % ProcessInfoArray[nextProcess].period)
+                                maxRemainingTimeProcess = j;
                         }
                     }
 
@@ -186,15 +195,20 @@ int main() {
                     // we need to switch to another process if needed
 
                     int minRemainingTimeProcess = -1;
-                    if (maxRemainingTimeProcess == -1 && urgentProcess == nextProcess){
+                    if (maxRemainingTimeProcess == -1 &&
+                        urgentProcess == nextProcess) {
                         for (int j = 0; j < NumOfProcesses; j++) {
-                            if (ProcessInfoArray[nextProcess].period - i % ProcessInfoArray[nextProcess].period ==
-                            ProcessInfoArray[j].period - i % ProcessInfoArray[j].period &&
-                            ProcessInfoArray[nextProcess].remaining_time > ProcessInfoArray[j].remaining_time){
-                                if (minRemainingTimeProcess == -1){
+                            if (ProcessInfoArray[nextProcess].period -
+                                i % ProcessInfoArray[nextProcess].period ==
+                                ProcessInfoArray[j].period -
+                                i % ProcessInfoArray[j].period &&
+                                ProcessInfoArray[nextProcess].remaining_time >
+                                ProcessInfoArray[j].remaining_time) {
+                                if (minRemainingTimeProcess == -1) {
                                     minRemainingTimeProcess = j;
-                                } else{
-                                    if (ProcessInfoArray[minRemainingTimeProcess].remaining_time > ProcessInfoArray[j].remaining_time){
+                                } else {
+                                    if (ProcessInfoArray[minRemainingTimeProcess].remaining_time >
+                                        ProcessInfoArray[j].remaining_time) {
                                         minRemainingTimeProcess = j;
                                     }
                                 }
@@ -202,34 +216,39 @@ int main() {
                         }
                     }
 
-                    if (maxRemainingTimeProcess != -1){
+                    if (maxRemainingTimeProcess != -1) {
                         urgentProcess = maxRemainingTimeProcess;
                     }
 
-                    if (minRemainingTimeProcess != -1){
+                    if (minRemainingTimeProcess != -1) {
                         urgentProcess = minRemainingTimeProcess;
                     }
 
                     // Do the preempt and start the next process
 
-                    if (urgentProcess != nextProcess && ProcessInfoArray[urgentProcess].remaining_time != 0){
-                        printf("%d: process %d preempted!\n", i, nextProcess+1);  // Preempted Message
+                    if (urgentProcess != nextProcess &&
+                        ProcessInfoArray[urgentProcess].remaining_time != 0) {
+                        printf("%d: process %d preempted!\n", i,
+                               nextProcess + 1);  // Preempted Message
                         nextProcess = urgentProcess;
-                        printf("%d: process %d starts\n", i, nextProcess+1); // Print the start message
+                        printf("%d: process %d starts\n", i,
+                               nextProcess + 1); // Print the start message
                     }
                 }
                 break; // End of case 4
             case 5:
                 // When coming to this case, the time is equal to some process's period
-                printf("%d: process %d missed deadline (%d ms left)\n", i, nextProcess+1, ProcessInfoArray[nextProcess].remaining_time);
-                for(int j = 0; j < NumOfProcesses; j++){
-                    if (i % ProcessInfoArray[j].period == 0){
+                printf("%d: process %d missed deadline (%d ms left)\n", i,
+                       nextProcess + 1,
+                       ProcessInfoArray[nextProcess].remaining_time);
+                for (int j = 0; j < NumOfProcesses; j++) {
+                    if (i % ProcessInfoArray[j].period == 0) {
                         ProcessInfoArray[j].remaining_time += ProcessInfoArray[j].burst_time;
                     }
                 }
 
                 // Choice a another process to run
-                if (nextProcess != -1){
+                if (nextProcess != -1) {
                     int urgentProcess = nextProcess;
                     for (int j = 0; j < NumOfProcesses; j++) {
                         // Pick the a more urgent process
@@ -243,10 +262,13 @@ int main() {
                         }
                     }
                     // Switch Process
-                    if (urgentProcess != nextProcess && ProcessInfoArray[urgentProcess].remaining_time != 0){
-                        printf("%d: process %d preempted!\n", i, nextProcess+1);  // Preempted Message
+                    if (urgentProcess != nextProcess &&
+                        ProcessInfoArray[urgentProcess].remaining_time != 0) {
+                        printf("%d: process %d preempted!\n", i,
+                               nextProcess + 1);  // Preempted Message
                         nextProcess = urgentProcess;
-                        printf("%d: process %d starts\n", i, nextProcess+1); // Print the start message
+                        printf("%d: process %d starts\n", i,
+                               nextProcess + 1); // Print the start message
                     }
                 }
                 break; // End of case 5
@@ -260,7 +282,8 @@ int main() {
     free(ProcessInfoArray); // Release the memory
     printf("Sum of all waiting times: %.0f\n", totalWaiting);
     printf("Number of CPU bursts: %.0f\n", cpuBurst);
-    float averageWaitingTime = totalWaiting / cpuBurst; // Calculate the average waiting time
+    float averageWaitingTime =
+            totalWaiting / cpuBurst; // Calculate the average waiting time
     printf("Average Waiting Time: %f\n", averageWaitingTime);
     return 0;
 }
